06685926091dac982825454f43d18cb419edd47c
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-08-19 15:40:54 +0300

    add navigation and some updates dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,10 +50,21 @@ android {
 }
 
 dependencies {
-    //AndroidX
-    implementation(libs.bundles.androidX)
-    //Compose
-    implementation(platform(libs.compose.bom))
-    debugImplementation(libs.compose.tooling)
-    implementation(libs.bundles.ui)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.navigation.compose)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }

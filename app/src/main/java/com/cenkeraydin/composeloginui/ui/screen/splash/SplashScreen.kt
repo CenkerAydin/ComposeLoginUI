0c8b2810ace8386b24c055a71066d24614840a6d
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-08-20 18:39:10 +0300

    design for splash screen

--- app/src/main/java/com/cenkeraydin/composeloginui/ui/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/cenkeraydin/composeloginui/ui/screen/splash/SplashScreen.kt
@@ -2,14 +2,25 @@ package com.cenkeraydin.composeloginui.ui.screen.splash
 
 import android.window.SplashScreen
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.cenkeraydin.composeloginui.R
@@ -26,8 +37,25 @@ fun SplashScreen(navController: NavController){
             contentScale = ContentScale.Crop,
             painter = painterResource(id = R.drawable.img_splash),
             contentDescription = "Logo"
+    )
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 150.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Welcome TaskMaster",
+            fontSize = 32.sp,
+            fontWeight = FontWeight(700),
+            style = TextStyle(
+                brush = Brush.linearGradient(
+                    colors = listOf(Color.Red, Color.Blue)
+                )
+            ),
+            color = Color.White
         )
-
+    }
 }
 @Composable
 @Preview

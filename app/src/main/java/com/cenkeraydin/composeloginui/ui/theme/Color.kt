ee211e09be4f06802a9f641b7ccd265a5eb1f8d1
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-09-02 13:07:49 +0300

    add new color

--- app/src/main/java/com/cenkeraydin/composeloginui/ui/theme/Color.kt
+++ app/src/main/java/com/cenkeraydin/composeloginui/ui/theme/Color.kt
@@ -6,26 +6,23 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 
-val PrimaryYellowLight = Color(0xFFffeed4)
-val PrimaryYellow = Color(0xFFFFE7BA)
-val PrimaryYellowDark = Color(0xFFe9c16c)
+val DarkPrimary = Color(0xFF36A2EB) // Vibrant blue
+val DarkSecondary = Color(0xFF5DD38A) // Fresh green
+val DarkTertiary = Color(0xFFEFB8C8) // Pink
+val DarkBackground = Color(0xFF1A1A2C) // Purple Dark
+val DarkSurface = Color(0xFF2D2D4D) // Dark gray
+val DarkOnSurface = Color(0xFFEAEAEA) // Light gray (Text and icons on the surface)
+val DarkSurfaceVariant = Color(0xFF26264D) // Slightly lighter purple
 
-val PrimaryPinkBlended = Color(0xFFe4b1cb)
-val PrimaryPinkLight = Color( 0xFFd6a0df)
-val PrimaryPink = Color(0xFFc073cd)
-val PrimaryPinkDark = Color(0xFF78258b)
-
-val PrimaryVioletLight = Color(0xFFb0a7e2)
-val PrimaryViolet = Color(0xFF9e91e2)
-val PrimaryVioletDark = Color(0xFF52439a)
-
-val PrimaryGreen = Color(0xFF00c29f)
-val PrimaryGreenDark = Color(0xFF008b6c)
-
-val DarkTextColor = Color(0xFF132b4c)
+val LightPrimary = Color(0xFF36A2EB) // Vibrant blue
+val LightSecondary = Color(0xFF5DD38A) // Fresh green
+val LightTertiary = Color(0xFFEFB8C8) // Pink
+val LightBackground = Color(0xFFF5F5F5) // Off-white
+val LightSurface = Color(0xFFE0E0E0) // Dark gray
+val LightOnSurface = Color(0xFF444444) // Dark gray (Text and icons on the surface)
+val LightSurfaceVariant = Color(0xFFD1D1E0) // Slightly lighter purpleval DarkTextColor = Color(0xFF132b4c)
